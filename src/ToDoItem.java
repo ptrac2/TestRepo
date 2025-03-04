978f6fb7338c398db8fb8e2f486ec09b503432df
ptrac2 <the.patrick.tracey@gmail.com>
2025-03-04 17:57:04 +1000

    Initial commit

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -1,2 +1,9 @@
-public class ToDoItem {
+class ToDoItem {
+    private String description;
+    private boolean isDone;
+
+    // TODO Now:
+    // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
+    // Add getters and setters for each field
 }
+
--- src/onLogIn.java
+++ src/onLogIn.java
@@ -1,2 +1,15 @@
-public class onLogIn {
+class Main {
+    // ... (other methods and fields)
+
+    public static void onLogIn() {
+        System.out.print("Enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+        User user = authService.logIn(username, password);
+        System.out.println("Welcome, " + user.getUsername() + "!");
+        // TODO Now: Create an instance of the ToDoList class with the logged-in user and call the run method
+    }
 }
+
